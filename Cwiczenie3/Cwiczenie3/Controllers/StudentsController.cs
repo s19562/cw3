ffbcdc91ceb594ef670a5e2d418a7e1afd762b6b
s19562 <49717167+s19562@users.noreply.github.com>
2020-03-18 02:47:50 +0100

    Create cw3 again

---
+++ Cwiczenie3/Cwiczenie3/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Cwiczenie3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kowalski , Malewski , Andrzejewski ";
+        }
+    }
+}
