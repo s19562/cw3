c8d2cb1c34faa3ac7b9a62d4a6f1950af5bdd477
s19562 <s19562@pjwstk.edu.pl>
2020-04-05 14:52:44 +0200

    Create EnrollmentsController.cs

---
+++ Cwiczenie3/Cwiczenie3/Controllers/EnrollmentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenie3.Controllers
+{
+    [Route("api/enrollents/promotions")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Enrollments()
+        {
+            return View();
+        }
+    }
+}
