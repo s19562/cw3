b10951e620704158c1a6087c72ef9e3eae815d1b
s19562 <s19562@pjwstk.edu.pl>
2020-04-19 21:47:21 +0200

    5++
    zapomniany commit

--- Cwiczenie3/Cwiczenie3/Models/EnrollmentOdp.cs
+++ Cwiczenie3/Cwiczenie3/Models/EnrollmentOdp.cs
@@ -5,7 +5,11 @@ using System.Threading.Tasks;
 
 namespace Cwiczenie3.Models
 {
-    public class EnrollmentResponse
+    public class EnrollmentOdp
     {
+        public int IdEnrollment { get; set; }
+        public int Semester { get; set; }
+        public int IdStudy { get; set; }
+        public String StartDate { get; set; }
     }
 }
